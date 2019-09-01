37871de2c4e8258c9bf49edfc8a06097586450c3
Reed Kavner <rik919@gmail.com>
2019-09-01 19:05:27 -0400

    added websockets and support for text posts

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-$( document ).ready(function() {
-    
-    // TODO establish websocket connection
+$(document).ready(function() {
 
     // Config constants
     const nameHeight = 20; // height of the slackName div in pixels
@@ -10,15 +8,26 @@ $( document ).ready(function() {
     const intervalReset = 10; // time in seconds for when a users gif displays at full scale
 
     const $main = $('#main');
-    const $prompt = $('#prompt');
 
-    // dummy incoming json
-    const testGif = {
-        'gifsrc' : 'https://media.giphy.com/media/mCRJDo24UvJMA/giphy.gif',
-        'width' : 500,
-        'height' : 362
-    };
-    
+    const connection = new signalR.HubConnectionBuilder()
+        .withUrl(`https://xoxo-closing-party.azurewebsites.net/api`)
+        .configureLogging(signalR.LogLevel.Information)
+        .build();
+
+    // TODO make this call an actual function
+    connection.on('newMessage', function(j) {
+        console.log('got a message!');
+        console.log(j.url);
+        addPost(j);
+    });
+
+    connection.onclose(() => console.log('disconnected'));
+
+    console.log('connecting...');
+    connection.start()
+        .then(() => console.log("Connected!"))
+        .catch(console.error);
+
     // get window size
     var windowHeight = $(window).height();
     var windowWidth = $(window).width();
@@ -29,31 +38,32 @@ $( document ).ready(function() {
     var users = {};
 
     /// get currect epoch time in seconds
-    function timeNow(){
+    function timeNow() {
         var now = new Date();
         return Math.round(now.getTime() / 1000);
     }
 
     // generate a random position
-    function randomPosition(j, scale){
-        var totalHeight = j.height + nameHeight;
+    function randomPosition(height, width, scale) {
+        var totalHeight = height + nameHeight;
         var coords = {
-            top : Math.floor( Math.random() * ( windowHeight - (scale * totalHeight) ) - ((totalHeight/2) * (1-scale)) ),
-            right : Math.floor( Math.random() * ( windowWidth - (scale * j.width) ) - ((j.width/2) * (1-scale)) )
+            top: Math.floor(Math.random() * (windowHeight - (scale * totalHeight)) - ((totalHeight / 2) * (1 - scale))),
+            right: Math.floor(Math.random() * (windowWidth - (scale * width)) - ((width / 2) * (1 - scale)))
         };
         return coords;
     }
 
-    function addGif(j){
+    function addPost(j) {
+        var username = j.user;
         // get users last posted time
-        var lastPosted = users[j.username];
+        var lastPosted = users[username];
 
-        // TODO set this depending on users last posted time
         var scale;
-        if (lastPosted === undefined){
+
+        if (lastPosted === undefined) {
             console.log('new user');
             scale = maxScale;
-        }else{
+        } else {
             var userInterval = timeNow() - lastPosted;
             scale = userInterval / intervalReset;
             scale = Math.min(Math.max(scale, minScale), maxScale);
@@ -63,63 +73,82 @@ $( document ).ready(function() {
         // set the animation duration based on scale
         var duration = (baseDuration * scale).toString() + 's';
 
-        // get the random position where we'll place this gif
-        var coords = randomPosition(j, scale);
+        // make the container element
+        var $div = $("<div>", { "class": "post-container" });
+        var coords;
+        var $content;
+        
+        // determine what kind of content
+        // TODO DRY this up with a styleContainer function
+        if ('url' in j) {
+            // it's a GIF!!
+            $content = $("<img>", { "class": "gif post-content", "src": j.url });
+            coords = randomPosition(j.height, j.width, scale);
+            $div.css({
+                'width': (j.width).toString() + 'px',
+                'height': (j.height + nameHeight).toString() + 'px'
+            });
+        } else if ('text' in j) {
+            // it's text!!!
+            $content = $("<div>", { "class": "text post-content" }).text(j.text);
+            var coords = randomPosition(j, scale);
+            coords = randomPosition(500, 600, scale);
+            $div.css({
+                'max-width': '500px',
+                'max-height': '600px'
+            });
+        }
 
-        // make the container element and add styling
-        var $div = $("<div>", {"class": "gif-container"});
         $div.css({
-            'transform' : 'scale(' + scale + ')', 
-            'animation-duration' : duration,
-            'top' : coords.top,
-            'left': coords.right,
-            'width': (j.width).toString() + 'px',
-            'height': (j.width + nameHeight).toString() + 'px'
+            'transform': 'scale(' + scale + ')',
+            'animation-duration': duration,
+            'top': coords.top,
+            'left': coords.right
         });
 
-        // make the other elements and put em together
-        var $slackname = $("<div>", {"class": "slack-name"}).text(j.username);
-        var $gif = $("<img>", {"class": "gif", "src": j.gifsrc});
-        $div.append($gif).append($slackname);
+        // create name label and add it and the the content to the container div
+        var $slackname = $("<div>", { "class": "slack-name" }).text(username);
+        $div.append($content).append($slackname);
 
         // display gif/container only once gif has loaded
-        $gif.on('load', function(){
-            $(this).parent().css({'opacity': 1}).addClass('shrinking');
-            console.log('showing gif');
-        });
+        // if it's a text post, just show the text
+        // TODO handle emoji
+        if ($content.hasClass('gif')) {
+            $content.on('load', function() {
+                $div.css({ 'opacity': 1 }).addClass('shrinking');
+                console.log('showing gif');
+            });
+        } else {
+            $div.css({ 'opacity': 1 }).addClass('shrinking');
+        }
 
         // remove from dom when animation finishes
-        $div.on('animationend', function(){
+        $div.on('animationend', function() {
             console.log('animationend');
             $(this).remove();
-
-            // see if the there are any gifs on the page
-            // if not, show the prompt after a few seconds
-            if ( $('.gif-container').length === 0){
-                setTimeout(function(){
-                    // make sure this is still true
-                    if ( $('.gif-container').length === 0){
-                        $prompt.removeClass('hidden');
-                    }
-                }, 2000);
-            }
         });
 
-        // hide the prompt
-        $prompt.addClass('hidden');
-
         // set current time for user
-        users[j.username] = timeNow();
-        
+        users[username] = timeNow();
+
         // add the post to the page
         $main.append($div);
-
     }
 
     // test add new gif
-    $(document).keypress(function(e){
-        var testInput = testGif;
-        testGif['username'] = 'xoxo person ' + e.key;
-        addGif(testInput);
+    $(document).keypress(function(e) {
+        if (isNaN(e.key)) {
+            var testPost = {
+                'url': 'https://media.giphy.com/media/mCRJDo24UvJMA/giphy.gif',
+                'width': 500,
+                'height': 362
+            };
+        }else{
+            var testPost = {
+                'text' : 'When the moon hits your eye like a big pizza pie'
+            };
+        }
+        testPost['user'] = 'xoxo person ' + e.key;
+        addPost(testPost);
     });
 });
